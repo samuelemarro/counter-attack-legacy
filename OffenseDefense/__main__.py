Completed train_approximator. Completed load_partial_state_dict. Added get_last_layer. Packaged normalisation handling in "apply_normalisation". Fixed generate_approximation_dataset treating predictions as labels. Added some documentation and debug logging.

OffenseDefense/__main__.py
@@ -3,6 +3,7 @@ import logging
 import click
 import foolbox
 import numpy as np
+import pathlib
 import sklearn
 import sys
 import torch
@@ -24,22 +25,29 @@ import OffenseDefense.utils as utils
 logger = logging.getLogger('OffenseDefense')
 
 # TODO: Test preprocessing options
-# TODO: Save models, not weights
 # TODO: Allow for optional model weights?
 # TODO: Check that the pretrained model does not contain normalisation inside?
 # TODO: British vs American spelling
 # TODO: Check composite workers
-# TODO: load_partial_state_dict and get_torch_model() with optional n+1 classes
-# TODO: Finish train_approximator
 # TODO: Download the cifar100 weights for densenet-bc-100-12 (when available)
 # TODO: Upload both of them and update the links in config.ini
 # TODO: counter_attack_workers doesn't do anything!
+# TODO: Sanity check: Difference between the original model and its trained approximator
+# TODO: Verify that the transfer is successful
+# TODO: Complete the substitutes
+# TODO: In pretrained_model, you are passing the model path, not the weights one
+# TODO: 99.99% accuracy when evaluating the model for attack? Seems fishy
 
 # IMPORTANT:
 # Shallow attacks the standard model, then it is evaluated on the defended model
 # Substitute and Black-Box attack the defended model
 # This means that you cannot write the sanity check "Shallow is the same as
 # a Substitute that uses the original as gradient estimator"
+
+# Note: Not all adversarial attacks are successful. This means that an approximation
+# dataset will have slightly less adversarial samples. This unbalanced dataset should
+# not cause problems when training approximators, but it might cause problems when
+# training classifiers.
 
 @click.group()
 def main(*args):
@@ -182,6 +190,23 @@ def accuracy(options, top_ks):
 def detector_roc(options, score_dataset_path, no_test_warning):
     """
     Uses a detector to identify adversarial samples and computes the ROC curve.
+
+    \b
+    Stores the following results:
+        ROC Area Under Curve (ROC-AUC)
+        Best Threshold: The threshold with the best Youden Index (TPR - FPR)
+        Best Threshold True Positive Rate: The TPR at the best threshold
+        Best Threshold False Positive Rate: The FPR at the best threshold
+
+        Genuine Scores: All the scores computed for the genuine samples
+        Adversarial Scores: All the scores computed for the adversarial samples
+
+    The last three columns contain the data to build the ROC curve. These are:
+        Thresholds
+        True Positive Rates
+        False Positive Rates
+
+    Each threshold has a corresponding TPR and FPR.
     """
 
     adversarial_loader = options['adversarial_loader']
@@ -213,8 +238,8 @@ def detector_roc(options, score_dataset_path, no_test_warning):
 
     info = [['ROC AUC', '{:2.2f}%'.format(area_under_curve * 100.0)],
             ['Best Threshold', '{:2.2e}'.format(best_threshold)],
-            ['Best True Positive Rate', '{:2.2f}%'.format(best_tpr * 100.0)],
-            ['Best False Positive Rate', '{:2.2f}%'.format(best_fpr * 100.0)]]
+            ['Best Threshold True Positive Rate', '{:2.2f}%'.format(best_tpr * 100.0)],
+            ['Best Threshold False Positive Rate', '{:2.2f}%'.format(best_fpr * 100.0)]]
 
     header = ['Genuine Scores', 'Adversarial Scores',
               'Thresholds', 'True Positive Rates', 'False Positive Rates']
@@ -252,11 +277,17 @@ def detector_roc(options, score_dataset_path, no_test_warning):
 
 @main.group()
 def defense():
+    """
+    Use defenses against various attack strategies.
+    """
     pass
 
 
 @defense.group(name='rejector')
 def rejector_defense():
+    """
+    Defend using a "rejector", which is a tool (usually a detector) that rejects adversarial samples.
+    """
     pass
 
 
@@ -386,6 +417,9 @@ def black_box_rejector(options):
 
 @defense.group(name='model')
 def model_defense():
+    """
+    Defend using a custom model.
+    """
     pass
 
 
@@ -494,6 +528,9 @@ def black_box_model(options):
 
 @defense.group(name='preprocessor')
 def preprocessor_defense():
+    """
+    Defend using a "preprocessor", which modifies the image before passing it to the model.
+    """
     pass
 
 
@@ -754,7 +791,7 @@ def train_model(options, trained_model_path):
         trained_model_path = parsing.get_training_default_path(
             'train_model', dataset, start_time)
 
-    torch_model = parsing._get_torch_model(dataset)
+    torch_model = parsing.get_torch_model(dataset)
     torch_model.train()
 
     if cuda:
@@ -850,33 +887,65 @@ def approximation_dataset_rejector(options):
 @main.command()
 @parsing.global_options
 @parsing.train_options
-@parsing.standard_model_options
+@click.argument('defense_type', type=click.Choice(['model', 'preprocessor', 'rejector']))
 @click.argument('approximation_dataset_path', type=click.Path(exists=True, file_okay=True, dir_okay=False))
+@click.option('--base-weights-path', type=click.Path(exists=True, file_okay=True, dir_okay=False), default=None,
+    help='The path to the file where the base weights are stored. If unspecified, it defaults to the pretrained model for the dataset.')
+@click.option('--normalisation', default=None,
+            help='The normalisation that will be applied by the model. Supports both dataset names ({}) and '
+            'channel stds-means (format: "red_mean green_mean blue_mean red_stdev green_stdev blue_stdev" including quotes).'.format(', '.join(parsing.datasets)))
 @click.option('--trained-approximator-path', type=click.Path(exists=False, file_okay=True, dir_okay=False), default=None,
-              help='The path to the file where the approximator will be saved. If unspecified, it defaults to \'./trained_models/train_approximator/$dataset$ $start_time$.pth.tar\'')
-def train_approximator(options, target_model_path, trained_approximator_path):
-    base_model = options['base_model']
+              help='The path to the file where the approximator will be saved. If unspecified, it defaults to \'./trained_models/train_approximator/$defense_type$/$dataset$ $start_time$.pth.tar\'')
+def train_approximator(options, defense_type, approximation_dataset_path, base_weights_path, normalisation, trained_approximator_path):
+    batch_size = options['batch_size']
     cuda = options['cuda']
     dataset = options['dataset']
     epochs = options['epochs']
-    loader = options['loader']
-    optimiser_name = options['optimiser']
+    max_batches = options['max_batches']
+    optimiser_name = options['optimiser_name']
+    shuffle = options['shuffle']
     start_time = options['start_time']
+
+    if base_weights_path is None:
+        base_weights_path = './pretrained_models/' + dataset + '_weights.pth.tar'
 
     if trained_approximator_path is None:
         trained_approximator_path = parsing.get_training_default_path(
-            'train_approximator', dataset, start_time)
+            'train_approximator/' + defense_type, dataset, start_time)
+
+    is_rejector = defense_type == 'rejector'
+    model = parsing.get_torch_model(dataset, is_rejector)
+    model_tools.load_partial_state_dict(model, base_weights_path)
+    last_layer = model_tools.get_last_layer(model)
+
+    model = parsing.apply_normalisation(model, normalisation, 'model', '--normalisation')
+
+    model.train()
+
+    if cuda:
+        model.cuda()
+
+    last_layer.reset_parameters()
+
+    optimiser = parsing.build_optimiser(optimiser_name, last_layer.parameters(), options)
 
     # Build the pretrained model and the final model (which might be n+1)
     # Transfer some layers (which? how?)
     # Train the remaining layers of the final model
     # Save the model
 
+    approximation_data = utils.load_zip(approximation_dataset_path)
+    loader = loaders.ListLoader(approximation_data, batch_size, shuffle)
 
-    training.train_torch(torch_model, loader, torch.nn.CrossEntropyLoss(),
+    if max_batches is not None:
+        loader = loaders.MaxBatchLoader(loader, max_batches)
+
+    training.train_torch(model, loader, torch.nn.CrossEntropyLoss(),
                          optimiser, epochs, cuda, classification=True)
 
-    torch.save(torch_model, trained_approximator_path)
+    pathlib.Path(trained_approximator_path).parent.mkdir(parents=True, exist_ok=True)
+
+    torch.save(model, trained_approximator_path)
 
 
 if __name__ == '__main__':

OffenseDefense/cifar_models/densenet.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import math
 
 
-__all__ = ['densenet']
+__all__ = ['densenet', 'DenseNet']
 
 
 from torch.autograd import Variable

OffenseDefense/model_tools.py
@@ -1,3 +1,4 @@
+import logging
 import pathlib
 
 import foolbox
@@ -8,7 +9,9 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import numpy as np
 
-from . import utils
+from . import cifar_models, utils
+
+logger = logging.getLogger(__name__)
 
 def max_batch_predictions(foolbox_model, images, max_batch_size):
     batch_predictions = []
@@ -76,12 +79,28 @@ def save_state_dict(model, path):
     torch.save(model.state_dict(), path)
 
 def load_partial_state_dict(model, path):
-    pass
-    # TODO:
-    # Find all the keys in the target
-    # Load the dict key by key
-    # If there's a key or shape mismatch, skip
-    # Return all parameters that were not updated
+    original_dict = model.state_dict()
+    loaded_dict = torch.load(path)
+
+    copied_keys = []
+
+    for loaded_key in loaded_dict.keys():
+        assert loaded_key not in copied_keys
+
+        if loaded_key in original_dict.keys():
+            original_value = original_dict[loaded_key]
+            loaded_value = loaded_dict[loaded_key]
+
+            if original_value.shape == loaded_value.shape:
+                original_value.copy_(loaded_value)
+                copied_keys.append(loaded_key)
+            else:
+                logger.debug('Parameter mismatch for {} between original ({}) and loaded ({})'.format(loaded_key, original_value.shape, loaded_value.shape))
+        else:
+            logger.debug('No matches for {}')
+
+    unchanged_keys = [x for x in original_dict.keys() if x not in copied_keys]
+    logger.debug('Parameters not changed: {}'.format(unchanged_keys))
 
 def has_normalisation(module):
     assert isinstance(module, torch.nn.Module)
@@ -96,3 +115,11 @@ def has_normalisation(module):
             return True
 
     return False
+
+def get_last_layer(model):
+    if isinstance(model, torchvision.models.DenseNet):
+        return model.classifier
+    elif isinstance(model, cifar_models.DenseNet):
+        return model.fc
+    else:
+        raise NotImplementedError()

OffenseDefense/parsing.py
@@ -182,8 +182,8 @@ def _get_genuine_loaders(dataset, path, batch_size, shuffle, num_workers, downlo
     else:
         raise ValueError('Dataset not supported.')
 
-    train_loader = loaders.TorchLoader(train_loader)
-    test_loader = loaders.TorchLoader(test_loader)
+    train_loader = loaders.TorchLoaderWrapper(train_loader)
+    test_loader = loaders.TorchLoaderWrapper(test_loader)
 
     return train_loader, test_loader
 
@@ -203,7 +203,7 @@ def _download_pretrained_model(dataset, path):
         raise ValueError('Dataset not supported.')
 
 
-def _get_torch_model(dataset: str) -> torch.nn.Module:
+def get_torch_model(dataset: str, is_rejector=False) -> torch.nn.Module:
     """Returns the pretrained Torch model for a given dataset.
 
     Parameters
@@ -211,6 +211,8 @@ def _get_torch_model(dataset: str) -> torch.nn.Module:
     dataset : str
         The name of the dataset. Currently supported values
         are ['cifar10', 'cifar100', 'imagenet']
+    is_rejector : bool
+        If true, it adds an extra class, 'adversarial'
 
     Raises
     ------
@@ -232,14 +234,17 @@ def _get_torch_model(dataset: str) -> torch.nn.Module:
     else:
         raise ValueError('Dataset not supported')
 
+    if is_rejector:
+        num_classes = num_classes + 1
+
     # Use the models that have shown the best top-1 accuracy
     if dataset in ['cifar10', 'cifar100']:
-        # For CIFAR10(0), we use a DenseNet with depth 190 and growth rate 40
+        # For CIFAR10(0), we use a DenseNet with depth 100 and growth rate 12
         model = cifar_models.densenet(
-            depth=190, growthRate=40, num_classes=num_classes)
+            depth=100, growthRate=12, num_classes=num_classes)
     elif dataset == 'imagenet':
         # For ImageNet, we use a Densenet with depth 161 and growth rate 48
-        model = torchvision.models.densenet161(pretrained=False)
+        model = torchvision.models.densenet161(num_classes=num_classes, pretrained=False)
     else:
         raise ValueError('Dataset not supported.')
 
@@ -277,11 +282,11 @@ def _get_pretrained_torch_model(dataset: str, base_model: torch.nn.Module, path:
     # We load the model structure, too
 
     path = pathlib.Path(path)
+    state_dict_path = path.with_name(path.name.split('.')[0] + '_weights' + ''.join(path.suffixes))
+
     if not path.exists():
         if download:
-            base_model = _get_torch_model(dataset)
             path.parent.mkdir(parents=True, exist_ok=True)
-            state_dict_path = path.with_name(path.name.split('.')[0] + '_weights' + ''.join(path.suffixes))
             _download_pretrained_model(dataset, str(state_dict_path))
             model_tools.load_state_dict(base_model, state_dict_path, False, False)
             torch.save(base_model, str(path))
@@ -289,12 +294,12 @@ def _get_pretrained_torch_model(dataset: str, base_model: torch.nn.Module, path:
             raise RuntimeError(
                 'No pretrained model found: {}. Use --download-model to automatically download missing models.'.format(path))
 
-    # model = model_tools.load_state_dict(model, path, False, False)
-    model = torch.load(str(path))
+    model = model_tools.load_state_dict(base_model, str(state_dict_path), False, False)
+    # model = torch.load(str(path))
     return model
 
 
-def _get_normalisation(dataset: str) -> model_tools.Normalisation:
+def _get_normalisation_by_name(dataset: str) -> model_tools.Normalisation:
     """Returns the normalisation for a given dataset.
 
     Parameters
@@ -327,6 +332,39 @@ def _get_normalisation(dataset: str) -> model_tools.Normalisation:
         raise ValueError('Dataset not supported.')
 
     return model_tools.Normalisation(means, stds)
+
+def apply_normalisation(model, normalisation, model_name, option_name):
+    has_normalisation = model_tools.has_normalisation(model)
+
+    logger.debug('{} has normalisation: {}'.format(model_name, has_normalisation))
+
+    if not has_normalisation and normalisation is None:
+        logger.warning('You are not applying any mean/stdev normalisation to the {}. '
+                        'You can specify it by passing {} DATASET '
+                        'or {} "RED_MEAN BLUE_MEAN GREEN_MEAN RED_STDEV GREEN_STDEV BLUE_STDEV".'.format(model_name, option_name, option_name))
+
+    if has_normalisation and normalisation is not None:
+        logger.warning('You are applying mean/stdev normalisation to the {} multiple times.'.format(model_name))
+
+    if normalisation is not None:
+        logger.debug('Applying normalisation for the {}: {}'.format(model_name, normalisation))
+        try:
+            if normalisation in datasets:
+                normalisation_module = _get_normalisation_by_name(normalisation)
+            else:
+                values = normalisation.split(' ')
+                means = float(values[0]), float(
+                    values[1]), float(values[2])
+                stdevs = float(values[3]), float(
+                    values[4]), float(values[5])
+                normalisation_module = model_tools.Normalisation(means, stdevs)
+        except:
+            raise click.BadOptionUsage(option_name, 'Invalid normalisation format for the {}.'.format(model_name))
+
+        model = torch.nn.Sequential(
+            normalisation_module, model)
+
+    return model
 
 
 def _get_num_classes(dataset):
@@ -459,6 +497,8 @@ def global_options(func):
 
         num_classes = _get_num_classes(dataset)
 
+        logger.debug('CUDA is supported: {}'.format(torch.cuda.is_available()))
+
         cuda = torch.cuda.is_available() and not no_cuda
 
         device = torch.cuda.current_device() if cuda else 'cpu'
@@ -489,7 +529,7 @@ def standard_model_options(func):
     def _parse_standard_model_options(options, *args, **kwargs):
         dataset = options['dataset']
 
-        base_model = _get_torch_model(dataset)
+        base_model = get_torch_model(dataset)
 
         standard_model_options = dict(options)
         standard_model_options['base_model'] = base_model
@@ -530,13 +570,13 @@ def pretrained_model_options(func):
         if weights_path is None:
             weights_path = './pretrained_models/' + dataset + '.pth.tar'
 
-        logger.debug('Loading weights from {}'.format(weights_path))
+        logger.debug('Loading pretrained weights from {}'.format(weights_path))
 
         torch_model = _get_pretrained_torch_model(
             dataset, base_model, weights_path, download_model)
 
         torch_model = torch.nn.Sequential(
-            _get_normalisation(dataset), torch_model)
+            _get_normalisation_by_name(dataset), torch_model)
 
         torch_model.eval()
 
@@ -580,38 +620,15 @@ def custom_model_options(func):
         if custom_model_path is None:
             logger.info('No custom architecture path passed. Using default architecture {}'.format(
                 default_architecture_names[dataset]))
-            custom_torch_model = _get_torch_model(dataset)
+            custom_torch_model = get_torch_model(dataset)
+            logger.debug('Loading weights from {}'.format(custom_weights_path))
             model_tools.load_state_dict(
                 custom_torch_model, custom_weights_path, False, False)
         else:
+            logger.debug('Loading model from {}'.format(custom_model_path))
             custom_torch_model = torch.load(custom_model_path)
 
-        has_normalisation = model_tools.has_normalisation(custom_torch_model)
-
-        if not has_normalisation and custom_model_normalisation is None:
-            logger.warning('You are not applying any mean/stdev normalisation to your custom model. '
-                           'You can specify it by passing --custom-model-normalisation DATASET '
-                           'or --custom-model-normalisation "RED_MEAN BLUE_MEAN GREEN_MEAN RED_STDEV GREEN_STDEV BLUE_STDEV".')
-
-        if has_normalisation and custom_model_normalisation is not None:
-            logger.warning('You are applying mean/stdev normalisation to the custom model multiple times.')
-
-        if custom_model_normalisation is not None:
-            try:
-                if custom_model_normalisation in datasets:
-                    custom_model_normalisation = _get_normalisation(custom_model_normalisation)
-                else:
-                    values = custom_model_normalisation.split(' ')
-                    means = float(values[0]), float(
-                        values[1]), float(values[2])
-                    stdevs = float(values[3]), float(
-                        values[4]), float(values[5])
-                    custom_model_normalisation = model_tools.Normalisation(means, stdevs)
-            except:
-                raise click.BadOptionUsage('--custom-model-normalisation', 'Invalid normalisation format for the custom model.')
-
-            custom_torch_model = torch.nn.Sequential(
-                custom_model_normalisation, custom_torch_model)
+        custom_torch_model = apply_normalisation(custom_torch_model, custom_model_normalisation, 'custom model', '--custom-model-normalisation')
 
         custom_torch_model.eval()
 
@@ -622,6 +639,7 @@ def custom_model_options(func):
             custom_torch_model, (0, 1), num_classes, channel_axis=3, device=device, preprocessing=(0, 1))
 
         if max_model_batch_size > 0:
+            logger.debug('Applying model batch limiting: {}'.format(max_model_batch_size))
             custom_foolbox_model = model_tools.MaxBatchModel(custom_foolbox_model, max_model_batch_size)
 
         custom_model_options = dict(options)
@@ -1005,32 +1023,7 @@ def substitute_options(func):
 
         substitute_torch_model = torch.load(substitute_model_path)
 
-        has_normalisation = model_tools.has_normalisation(substitute_torch_model)
-
-        if not has_normalisation and substitute_normalisation is None:
-            logger.warning('You are not applying any mean/stdev normalisation to your substitute model. '
-                           'You can specify it by passing --substitute-normalisation DATASET '
-                           'or --substitute-normalisation "RED_MEAN BLUE_MEAN GREEN_MEAN RED_STDEV GREEN_STDEV BLUE_STDEV".')
-
-        if has_normalisation and substitute_normalisation is not None:
-            logger.warning('You are applying mean/stdev normalisation to the substitute model multiple times.')
-
-        if substitute_normalisation is not None:
-            try:
-                if substitute_normalisation in datasets:
-                    substitute_normalisation = _get_normalisation(substitute_normalisation)
-                else:
-                    values = substitute_normalisation.split(' ')
-                    means = float(values[0]), float(
-                        values[1]), float(values[2])
-                    stdevs = float(values[3]), float(
-                        values[4]), float(values[5])
-                    substitute_normalisation = model_tools.Normalisation(means, stdevs)
-            except:
-                raise click.BadOptionUsage('--substitute-normalisation', 'Invalid normalisation format for the substitute model.')
-
-            substitute_torch_model = torch.nn.Sequential(
-                substitute_normalisation, substitute_torch_model)
+        substitute_torch_model = apply_normalisation(substitute_torch_model, substitute_normalisation, 'substitute model', '--substitute-normalisation')
 
         substitute_torch_model.eval()
 

OffenseDefense/training.py
@@ -89,14 +89,15 @@ def train_torch(model, loader, loss_fn, optimizer, epochs, use_cuda, classificat
 
     return average_loss.avg, top1_accuracy, top5_accuracy
 
-def generate_approximation_dataset(foolbox_model, loader, name='approximation Dataset'):
+def generate_approximation_dataset(foolbox_model, loader, name='Approximation Dataset'):
     dataset = []
     for images, _ in IncrementalBar(name).iter(loader):
         # Note: for genuine datasets, "_" is the labels. For
         # adversarial datasets, "_" is the source images. In both
         # cases we do not need them.
 
-        labels = foolbox_model.batch_predictions(images)
+        batch_predictions = foolbox_model.batch_predictions(images)
+        labels = np.argmax(batch_predictions, axis=1)
 
         dataset_batch = list(zip(images, labels))
         dataset += dataset_batch